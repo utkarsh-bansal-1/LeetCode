0cb450482d650cedde94543aab218133fe9652e9
Utkarsh Bansal <utkarshbansal24march@gmail.com>
2026-08-11 16:13:37 +0530

    Time: 231 ms (59.66%) | Memory: 10.5 MB (64.44%) - LeetSync

--- AllProblems/79-word-search/word-search.cpp
+++ AllProblems/79-word-search/word-search.cpp
@@ -5,12 +5,12 @@ class Solution {
     vector<pair<int,int>> dir={{1 , 0} , {0 , 1} , {-1 , 0} , {0 , -1}};
 
     private:
-    bool recFind(string& s , string& word , int i , int j , set<pair<int,int>>& sSet , int sSize , int wordSize ,vector<vector<char>>& board , int n , int m  ){
-        if(s==word){
+    bool recFind( string& word , int i , int j , int sSize , int wordSize ,vector<vector<char>>& board , int n , int m  ){
+        if(sSize==wordSize){
             return true;
         }
 
-        if(i>=n || j>=m || j<0 || i<0 || sSet.find({i , j})!=sSet.end()){
+        if(i>=n || j>=m || j<0 || i<0 ){
             return false;
         }
 
@@ -18,31 +18,31 @@ class Solution {
             return false;
         }
 
+
         // try all possible directions after adding current charachter . 
-        s.push_back(board[i][j]);
-        sSet.insert({i , j});
+        char temp=board[i][j];
+        board[i][j]='#';
+
         for(auto pr : dir){
-            if(recFind(s , word , i+pr.first , j+pr.second , sSet , sSize+1 , wordSize , board ,n , m )){
+            if(recFind( word , i+pr.first , j+pr.second ,  sSize+1 , wordSize , board ,n , m )){
                 return true;
             };
         }
-        sSet.erase({i , j});
-        s.pop_back();
+
+        board[i][j]=temp;
 
         return false;
     }
 
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        string s="";
         int n=board.size();
         int m=board[0].size();
-        set<pair<int,int>> sSet;
         int wordSize=word.size();
 
         for(int i=0 ; i<n ; i++){
             for(int j=0 ; j<m ; j++){
-                if(recFind( s,  word , i  , j , sSet , 0 , wordSize , board , n , m  )){
+                if(recFind( word , i  , j , 0 , wordSize , board , n , m  )){
                     return true;
                 }
             }
