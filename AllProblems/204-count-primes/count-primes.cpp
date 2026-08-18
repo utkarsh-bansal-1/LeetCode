6b9e2f66dd2153f767e2068b31593d9926e13c72
Utkarsh Bansal <utkarshbansal24march@gmail.com>
2026-08-18 07:19:24 +0530

    Time: 645 ms (5.02%) | Memory: 25.5 MB (22.00%) - LeetSync

--- AllProblems/204-count-primes/count-primes.cpp
+++ AllProblems/204-count-primes/count-primes.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     int countPrimes(int n) {
-       // to do that . where n belongs to less than or equal to 5*10^6 . we follow seive of erathnosis . 
-        vector<int> seive(n, true);
-        int ans=0;
-        for(int i=2 ; i<n ; i++){
-            if(seive[i]){
-                for(long j=1ll*i*i ; j<n ; j+=i){
-                    seive[j]=false;
+
+        if (n <= 2)
+            return 0;
+
+        // isPrime[i] represents number (2*i + 1)
+        int size = n / 2;
+        vector<bool> isPrime(size, true);
+
+        // index 0 represents number 1
+        isPrime[0] = false;
+
+        for (int i = 3; 1LL * i * i < n; i += 2) {
+
+            if (isPrime[i / 2]) {
+
+                for (long long j = 1LL * i * i; j < n; j += 2LL * i) {
+                    isPrime[j / 2] = false;
                 }
-                ans++;
             }
-        } 
-        
+        }
+
+        int ans = 1; // prime number 2
+
+        for (int i = 1; i < size; i++) {
+            if (isPrime[i])
+                ans++;
+        }
+
         return ans;
     }
 };
